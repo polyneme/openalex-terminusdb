23e26e51e92dbf85fdc9faec0677b2d4cd583780
Donny Winston <donny@polyneme.xyz>
2023-05-30 15:00:46 -0400

    group assets

--- openalex_terminusdb/deployment.py
+++ openalex_terminusdb/deployment.py
@@ -1,5 +1,7 @@
-from dagster import Definitions, define_asset_job, ScheduleDefinition
+from dagster import Definitions, define_asset_job, ScheduleDefinition, \
+    load_assets_from_package_module
 
+from openalex_terminusdb import etl
 from openalex_terminusdb.config import (
     get_s3_config,
     get_mongo_config,
@@ -13,10 +15,15 @@ configured_s3_resource = S3Resource(**get_s3_config())
 configured_mongo_resource = MongoResource(**get_mongo_config())
 configured_terminus_resource = TerminusResource(**get_terminus_config())
 
+etl_assets = load_assets_from_package_module(
+    etl,
+    group_name="etl",
+)
+
 defs = Definitions(
     sensors=[],
     jobs=[],
-    assets=[connections_okay],
+    assets=etl_assets,
     resources={
         "s3": configured_s3_resource,
         "mongo": configured_mongo_resource,
